be8ccfb4f3564200a1e2bc85d3feed6030935e2c
E1Patronn <164499636+E1Patronn@users.noreply.github.com>
2024-05-17 16:20:41 +0200

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -13,7 +13,7 @@ provider "google" {
 }
 
 resource "google_project" "my_project" {
-  name       = "EFFICOM-IAC"
+  name       = "efficom5annee"
   project_id = "${var.project}-${random_string.lower.result}"
   billing_account = var.billing_account
   auto_create_network = false
